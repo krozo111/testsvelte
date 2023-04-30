67c0f19dcf4cbbe6de67c75a8b8e0984792a1777
Jorge Corzo <jorgeach111@hotmail.com>
2023-04-30 11:41:58 +0930

    .

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,16 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Platform {
+			env?: {
+				YOUR_KV_NAMESPACE: KVNamespace;
+				YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace;
+			};
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
+		}
 	}
 }
 

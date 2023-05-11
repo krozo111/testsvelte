ad8ff0a5f65e0d57ca6a4ed26a641af7b983fb80
Jorge Corzo <jorgeach111@hotmail.com>
2023-05-11 15:16:35 +0930

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <main class="container">
   <h1>First page testing May 11st!</h1>
   <h1>Hello and welcome to my site!</h1>
+  <div><a href="https://pointscalculator.pages.dev/">Points calculator (Australia)</a></div>
   <a href="/about">About my site</a>
 </main>
 

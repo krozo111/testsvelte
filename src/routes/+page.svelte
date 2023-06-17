5027ac1bf525cc306f942fd98827a9a58656570b
Jorge Corzo <jorgeach111@hotmail.com>
2023-06-17 23:09:34 +0930

    ads.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,33 @@
+<script>
+  import DecisionTree from "./DecisionTree.svelte";
+
+  const tree = [
+    {
+      question: "Is it a mammal?",
+      trueBranch: {
+        question: "Does it have fur?",
+        trueBranch: "Dog",
+        falseBranch: "Whale"
+      },
+      falseBranch: {
+        question: "Is it a bird?",
+        trueBranch: "Penguin",
+        falseBranch: "Snake"
+      }
+    }
+  ];
+</script>
+
+
+
 <main class="container">
-  <h1>First page testing May 11st!</h1>
+  <DecisionTree tree={tree} />
+  <h1>First page testing June 17!</h1>
   <h1>Hello and welcome to my site!</h1>
   <div><a href="https://pointscalculator.pages.dev/">Points calculator (Australia)</a></div>
   <a href="/about">About my site</a>
+
+  
 </main>
 
 <style>
---
+++ src/routes/DecisionTree.svelte
@@ -0,0 +1,29 @@
+<script>
+// @ts-nocheck
+
+
+    
+    export let tree;
+    let currentNode = tree[0];
+    let answer = null;
+  
+    function handleAnswer(yes) {
+      if (typeof currentNode.trueBranch === "string") {
+        answer = yes ? currentNode.trueBranch : currentNode.falseBranch;
+        return;
+      }
+  
+      currentNode = yes ? currentNode.trueBranch : currentNode.falseBranch;
+    }
+  </script>
+  
+  <main>
+    {#if answer !== null}
+      <h2>The answer is {answer}</h2>
+    {:else}
+      <h2>{currentNode.question}</h2>
+      <button on:click={() => handleAnswer(true)}>Yes</button>
+      <button on:click={() => handleAnswer(false)}>No</button>
+    {/if}
+  </main>
+  

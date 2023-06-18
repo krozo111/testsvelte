dd98587de16731699d8ed4aca6b25d6202e645bf
Jorge Corzo <jorgeach111@hotmail.com>
2023-06-18 10:32:18 +0930

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,15 @@
 <script>
-  import DecisionTree from "./DecisionTree.svelte";
-
-  const tree = [
-    {
-      question: "Is it a mammal?",
-      trueBranch: {
-        question: "Does it have fur?",
-        trueBranch: "Dog",
-        falseBranch: "Whale"
-      },
-      falseBranch: {
-        question: "Is it a bird?",
-        trueBranch: "Penguin",
-        falseBranch: "Snake"
-      }
-    }
-  ];
+  import Header from "$lib/components/header.svelte";
 </script>
 
-
-
 <main class="container">
-  <DecisionTree tree={tree} />
-  <h1>First page testing June 17!</h1>
-  <h1>Hello and welcome to my site!</h1>
-  <div><a href="https://pointscalculator.pages.dev/">Points calculator (Australia)</a></div>
+  <Header />
+  <div>
+    <a href="https://pointscalculator.pages.dev/"
+      >Points calculator (Australia)</a
+    >
+  </div>
   <a href="/about">About my site</a>
-
-  
 </main>
 
 <style>
